36adcce9d5007e3e87bbf235b2e5e73c6b3afd1d
HARIKA <157002815+wharika18@users.noreply.github.com>
2024-05-08 22:37:42 +0530

    Update DeployToken.s.sol

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -8,6 +8,5 @@ contract DeployToken {
 
     function run() external returns (TokenERC20) {
         TokenERC20 token = new TokenERC20(INITIAL_SUPPLY, "MyToken", "MTK");
-        return token; // so, while deployin itself you pass the args.. but when u use the oz erc20 inheritance, then in the contract.sol file itself, u giv ethe args and these can be directly used in the deploy script. but here as the constructor while deploying take sthe inputs, we can give in the deploy script by the way of args..
-    }
+        return token; 
 }
